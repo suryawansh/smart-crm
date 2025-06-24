774bb6e660d858ea9377b27c16d0d66007e6ff8c
Vedant Shankar Bhavsar <vedantbhavsar.a10@gmail.com>
2025-06-24 14:43:53 +0530

    feat: added authentication

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button>Click on</Button>
+      <Input placeholder="This is placeholder." />
+    </div>
+  );
+}
